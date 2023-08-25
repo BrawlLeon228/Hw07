f66639444e24a3f2e7075fdcdf2707b283d7493e
Dmitriy Tikhomirov <ytude7042@gmail.com>
2023-08-25 23:19:15 +0700

    a

---
+++ #47/Program.cs
@@ -0,0 +1,22 @@
+﻿void WriteArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) Console.Write(matr[i,j]+" ");
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) matr[i,j] = new Random().Next(-10.0,10.0);
+    }
+    WriteArray(matr);
+}
+
+int m = 3;
+int n = 4;
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
---
+++ #50/Program.cs
@@ -0,0 +1,21 @@
+﻿void ShowEl(int[,] matr, int m, int n)
+{
+    if (m <= matr.GetLength(0))
+    {   
+        if (n <= matr.GetLength(1)){
+            Console.WriteLine(matr[m-1, n-1]);
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Такого элемента в массиве нет");
+    }
+}
+
+int[,] matrix = new int[,]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+};
+ShowEl(matrix, 3, 2);
